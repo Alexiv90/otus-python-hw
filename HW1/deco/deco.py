52bdc4c97b6048913e2cb4cb6e41b2e4b12aea3f
alexiv90 <aaivanchenko90@yandex.ru>
2024-06-11 12:32:16 +0300

    add HW1

---
+++ HW1/deco/deco.py
@@ -0,0 +1,147 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import functools
+from functools import update_wrapper, wraps
+
+count = {}
+
+
+def disable():
+    '''
+    Disable a decorator by re-assigning the decorator's name
+    to this function. For example, to turn off memoization:
+
+    >>> memo = disable
+
+    '''
+    return
+
+
+def decorator():
+    '''
+    Decorate a decorator so that it inherits the docstrings
+    and stuff from the function it's decorating.
+    '''
+    return
+
+
+def countcalls(func):
+    '''Decorator that counts calls made to the function decorated.'''
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        wrapper.calls += 1
+        return func(*args, **kwargs)
+    wrapper.calls = 0
+    return wrapper
+
+
+def memo(func):
+    '''
+    Memoize a function so that it caches all return values for
+    faster future lookups.
+    '''
+    cache = {}
+
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        cache_key = args + tuple(kwargs.items())
+        if cache_key not in cache:
+            cache[cache_key] = func(*args, **kwargs)
+        return cache[cache_key]
+
+    return wrapper
+
+
+def n_ary(func):
+    '''
+    Given binary function f(x, y), return an n_ary function such
+    that f(x, y, z) = f(x, f(y,z)), etc. Also allow f(x) = x.
+    '''
+    def wrapper(*args):
+        value = args[0]
+        for i in args[1:]:
+            value = func(value, i)
+        return value
+    return wrapper
+
+
+def trace(zz):
+    '''Trace calls made to function decorated.
+
+    @trace("____")
+    def fib(n):
+        ....
+
+    >>> fib(3)
+     --> fib(3)
+    ____ --> fib(2)
+    ________ --> fib(1)
+    ________ <-- fib(1) == 1
+    ________ --> fib(0)
+    ________ <-- fib(0) == 1
+    ____ <-- fib(2) == 2
+    ____ --> fib(1)
+    ____ <-- fib(1) == 1
+     <-- fib(3) == 3
+
+    '''
+
+    def inner(func):
+        @wraps(func)
+        def wrapper(*args, **kwargs):
+            print(f'{zz*wrapper.count} --> {func.__name__}({args[0]}) ')
+            wrapper.count += 1
+            res = func(*args, **kwargs)
+            if res:
+                wrapper.count -= 1
+                print(
+                    f'{zz*wrapper.count} <-- {func.__name__}({args[0]}) == {res}')
+
+            return res
+        wrapper.count = 0
+        return wrapper
+
+    return inner
+
+
+@countcalls
+@memo
+@n_ary
+def foo(a, b):
+    return a + b
+
+
+@countcalls
+@memo
+@n_ary
+def bar(a, b):
+    return a * b
+
+
+@countcalls
+@trace("####")
+@memo
+def fib(n):
+    """Some doc"""
+    return 1 if n <= 1 else fib(n-1) + fib(n-2)
+
+
+def main():
+    print(foo(4, 3))
+    print(foo(4, 3, 2))
+    print(foo(4, 3))
+    print("foo was called", foo.calls, "times")
+
+    print(bar(4, 3))
+    print(bar(4, 3, 2))
+    print(bar(4, 3, 2, 1))
+    print("bar was called", bar.calls, "times")
+
+    print(fib.__doc__)
+    fib(3)
+    print(fib.calls, 'calls made')
+
+
+if __name__ == '__main__':
+    main()
+    # print(foo(2, 3, 4))
---
+++ HW1/log_analyzer/log_analyzer.py
@@ -0,0 +1,263 @@
+import argparse
+import gzip
+import json
+import logging
+import os
+import re
+from statistics import median
+import sys
+import traceback
+from configparser import ConfigParser
+from datetime import datetime
+from collections import namedtuple
+from typing import Callable, Iterator, NamedTuple, Optional
+
+
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+
+# log_format ui_short '$remote_addr  $remote_user $http_x_real_ip [$time_local] "$request" '
+#                     '$status $body_bytes_sent "$http_referer" '
+#                     '"$http_user_agent" "$http_x_forwarded_for" "$http_X_REQUEST_ID" "$http_X_RB_USER" '
+#                     '$request_time';
+
+config = {
+    "REPORT_SIZE": 1000,
+    "REPORT_DIR": "./reports",
+    "LOG_DIR": "./log",
+    "REPORT_TEMPLATE": "report.html",
+    "MAX_ERROR_RATE": 0.8,
+}
+LastLogFile = namedtuple("LastLogFile", ["filename", "date"])
+LogLine = namedtuple("LogLine", ["url", "time"])
+LogData = namedtuple(
+    "LogData", ["url_data", "total_time", "total_count", "errors_count"]
+)
+
+
+def parse_arguments() -> argparse.Namespace:
+    parser = argparse.ArgumentParser(description="NGINX Log analyzer script")
+    parser.add_argument(
+        "--config",
+        dest="config_path",
+        default='./config',
+        help="Path to a config file",
+    )
+
+    args = parser.parse_args()
+    return args
+
+
+def get_config_values(init_config, config_path: str) -> ConfigParser:
+    config = ConfigParser()
+    if isinstance(init_config, dict):
+        config.read_dict({"DEFAULT": init_config})
+    else:
+        config.read(config_path+'/'+init_config)
+
+    return config
+
+
+def init_logging_config(filename: Optional[str] = None, level: str = "INFO") -> None:
+    try:
+        logging.basicConfig(
+            filename=filename,
+            filemode="a",
+            format="[%(asctime)s] %(levelname).1s %(message)s",
+            datefmt="%Y.%m.%d %H:%M:%S",
+            level=getattr(logging, level),
+        )
+    except TypeError:
+        logging.error("Error initializing the logging system")
+        traceback.print_stack()
+        return False
+
+    return True
+
+
+def get_the_last_log_file(config: ConfigParser) -> NamedTuple:
+    dt_format = "%Y%m%d"
+    dt_regex = re.compile(
+        r"^nginx-access-ui\.log-(?P<full_date>(?P<year>\d{4})(?P<month>\d{2})(?P<day>\d{2}))"
+    )
+    log_dir = config.get("DEFAULT", "LOG_DIR")
+    if not log_dir or not os.path.exists(log_dir) or not os.path.isdir(log_dir):
+        logging.error("Log directory was not found. LOG_DIR: {}".format(log_dir))
+        return None
+
+    last_filename = None
+    last_date = datetime.strptime("19700101", dt_format)
+
+    for filename in os.listdir(log_dir):
+        path = os.path.join(log_dir, filename)
+        m = dt_regex.match(filename)
+
+        try:
+            dt = datetime.strptime(m.group("full_date"), dt_format)
+        except ValueError:
+            logging.error(
+                "Bad log_date ({}) for log_file {}".format(
+                    filename, m.group("full_date")
+                )
+            )
+            continue
+
+        if dt and dt > last_date:
+            last_filename = path
+            last_date = dt
+
+    return LastLogFile(last_filename, last_date)
+
+
+def get_report_name(report_dt: datetime.date) -> Optional[str]:
+    return "report-{0}.{1}.{2}.html".format(
+        report_dt.year, report_dt.month, report_dt.day
+    )
+
+
+def check_report_exists(log_path: str, report_file: str) -> bool:
+    if report_file:
+        report_path = os.path.join(log_path, report_file)
+        if os.path.exists(report_path) and os.path.isfile(report_path):
+            return True
+    return False
+
+
+def get_open_log_func(filename: str) -> Callable:
+    if re.search(r"[.]gz$", filename):
+        return gzip.open
+    else:
+        return open
+
+
+def read_log_file(log_file: str, encoding: str = "utf-8") -> Iterator[str]:
+    open_f = get_open_log_func(log_file)
+    with open_f(log_file, encoding=encoding) as file:
+        for line in file:
+            yield line
+
+
+def handle_log_line(line: str) -> NamedTuple:
+    log_regexp = re.compile(r"\"\w+ (?P<url>(.*?)) HTTP.* (?P<time>[0-9.]+)$")
+
+    if m := log_regexp.search(str(line)):
+        return LogLine(m.group("url"), float(m.group("time")))
+
+    return LogLine(None, 0)
+
+
+def parse_logs(filename: str) -> NamedTuple:
+    url_data = {}
+    total_count = 0
+    total_time = 0
+    errors_count = 0
+
+    open_f = get_open_log_func(filename)
+    with open_f(filename, mode="r") as log_file:
+        for line in log_file:
+            url, time = handle_log_line(line)
+
+            if not (url and line):
+                errors_count += 1
+            total_count += 1
+            total_time += time
+
+            if url not in url_data:
+                url_data[url] = [time]
+            else:
+                url_data[url].append(time)
+
+    return LogData(url_data, total_time, total_count, errors_count)
+
+
+def handle_log_data(log_data: NamedTuple, config: ConfigParser) -> Optional[list]:
+    urls = log_data.url_data
+    result = []
+
+    error_rate = log_data.errors_count / log_data.total_count
+    logging.debug("The error rate in the log is - {}".format(round(error_rate, 3)))
+    max_error_rate = float(config.get("DEFAULT", "MAX_ERROR_RATE"))
+
+    if error_rate >= max_error_rate:
+        logging.error(
+            "Maximum error rate {} was exceeded ({})".format(max_error_rate, error_rate)
+        )
+        return None
+
+    for url in urls:
+        count = len(urls[url])
+        count_perc = 100 * float(count) / log_data.total_count
+        time_avg = sum(urls[url]) / count
+        time_max = max(urls[url])
+        time_med = median(sorted(urls[url]))
+        time_sum = sum(urls[url])
+        time_perc = 100 * time_sum / log_data.total_time
+        result.append(
+            {
+                "url": url,
+                "count": count,
+                "count_perc": round(count_perc, 3),
+                "time_avg": round(time_avg, 3),
+                "time_max": round(time_max, 3),
+                "time_med": round(time_med, 3),
+                "time_perc": round(time_perc, 3),
+                "time_sum": round(time_sum, 3),
+            }
+        )
+
+    result.sort(key=lambda res: res["time_sum"], reverse=True)
+    return result
+
+
+def fill_html_report(config: ConfigParser, filename: str, result_data: dict) -> bool:
+    template = config.get("DEFAULT", "REPORT_TEMPLATE")
+    report_file = "./{}/{}".format(config.get("DEFAULT", "REPORT_DIR"), filename)
+    print(template)
+    with open(template, mode="r") as template:
+        with open(report_file, mode="w") as report:
+            for line in template:
+                if "$table_json" in line:
+                    report.write(line.replace("$table_json", json.dumps(result_data)))
+                else:
+                    report.write(line)
+
+
+def main(report_config) -> None:
+    if not init_logging_config(level="DEBUG"):
+        sys.exit("Check init_logging_config() usage!")
+    last_log_file = get_the_last_log_file(report_config)
+
+    if not last_log_file:
+        sys.exit("Can't get last log file! Finishing script...")
+    logging.debug("The last log file is - {}".format(last_log_file.filename))
+
+    report_name = get_report_name(last_log_file.date)
+    logging.debug("Result file name will be - {}".format(report_name))
+
+    if check_report_exists(report_config.get("DEFAULT", "REPORT_DIR"), report_name):
+        sys.exit("The report file ({}) already exists".format(report_name))
+
+    log_data = parse_logs(last_log_file.filename)
+    if not log_data:
+        sys.exit(
+            "There are too many errors in parsing! Check log file format - {}".format(
+                last_log_file
+            )
+        )
+    logging.info("Log file has been parsed successfully...")
+
+    result = handle_log_data(log_data, report_config)
+    logging.info("Log data has been processed successfully...")
+
+    fill_html_report(report_config, report_name, result)
+    logging.info("Log analyzer script has finished the work!")
+
+
+if __name__ == "__main__":
+    args = parse_arguments()
+    if args.config_path and os.path.exists(args.config_path):
+        config = get_config_values(config, args.config_path)
+    else:
+        config = get_config_values('config.ini', args.config_path)
+    main(config)
